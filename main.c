0d3d9181ac936df38e4cfb02366fe8f81af97acc
T-unity <takadanaritoshi@gmail.com>
2022-12-12 00:39:06 +0900

    prototype declaration

--- main.c
+++ main.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-  int min, max, sum;
-  printf("最小値と最大値を , で区切って入力して下さい。:");
-  scanf("%d , %d" , &min, &max);
+int sum(void);
 
-  sum = (min + max) * (max - min + 1) / 2;
-  printf("%d - %d の合計は %d です。\n", min, max, sum);
+int main(void)
+{
+  sum();
   return 0;
 }
 
-// int main(void)
-// {
-//   int juice = 198, milk = 138, money = 1000;
-//   double tax = 1.05;
-//   money = 1000 - ((juice + (milk * 2)) * tax);
-//   printf("%d円\n", (int)money);
-// }
+int sum(void)
+{
+  printf( "%d\n", (100 + 100) * 100 );
+  return 0;
+}
